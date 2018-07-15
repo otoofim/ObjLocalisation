fe6f939e761b03dd4a249a341e897226bbd68381
otoofim <moh1371@gmail.com>
2018-07-15 16:30:54 +0100

    network works fine

--- Agent.py
+++ Agent.py
@@ -1,12 +1,14 @@
 
-# coding: utf-8
-
-# In[132]:
-
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
+
+
+# coding: utf-8
+
+# In[13]:
 
 
 # ACTIONS
@@ -51,13 +53,38 @@ class ObjLocaliser(object):
         #self.xmax =
         #self.ymin =
         #self.ymax =
-        
+        self.targets = self.gettingTargerReady(boundingBoxes)
         #Initializing sliding window from top left corner of the image
         #[x_top_left_corner, y_top_left_corner, x_down_right_corner, y_down_right_corner]
+        #self.agent_window = np.array([0,0,DIMENSION,DIMENSION])
         self.agent_window = np.array([0,0,100,50])
+        self.iou = 0
+
+        
+        self.actoins = {
+            0: 'MOVE_RIGHT',
+            1: 'MOVE_DOWN',
+            2: 'SCALE_UP',
+            3: 'ASPECT_RATIO_UP',
+            4: 'MOVE_LEFT',
+            5: 'MOVE_UP',
+            6: 'SCALE_DOWN',
+            7: 'ASPECT_RATIO_DOWN',
+            8: 'SPLIT_HORIZONTAL',
+            9: 'SPLIT_VERTICAL',
+            10: 'SKIP_REGION',
+            11: 'PLACE_LANDMARK'
+        }
         
         
         
+    def gettingTargerReady(self, boundingBoxes):
+        numOfObj = len(boundingBoxes['xmax'])
+        objs = []
+        for i in range(numOfObj):
+            temp = [boundingBoxes['xmin'][i], boundingBoxes['xmax'][i], boundingBoxes['ymin'][i], boundingBoxes['ymax'][i]]
+            objs.append(temp)
+        return objs
         
     def wrapping(self):
         
@@ -70,61 +97,73 @@ class ObjLocaliser(object):
     
     def takingActions(self,action):
 
+        newbox = np.array([0,0,0,0])
+        
         if action == MOVE_RIGHT:           
-            self.MoveRight()
+            newbox = self.MoveRight()
         elif action == MOVE_DOWN:         
-            self.MoveDown()
+            newbox = self.MoveDown()
         elif action == SCALE_UP:           
-            self.scaleUp()
+            newbox = self.scaleUp()
         elif action == ASPECT_RATIO_UP:    
-            self.aspectRatioUp()
+            newbox = self.aspectRatioUp()
         elif action == MOVE_LEFT:       
-            self.MoveLeft()
+            newbox = self.MoveLeft()
         elif action == MOVE_UP:       
-            self.MoveUp()
+            newbox = self.MoveUp()
         elif action == SCALE_DOWN:         
-            self.scaleDown()
+            newbox = self.scaleDown()
         elif action == ASPECT_RATIO_DOWN:  
-            self.aspectRatioDown()
+            newbox = self.aspectRatioDown()
         elif action == SPLIT_HORIZONTAL:   
-            self.splitHorizontal()
+            newbox = self.splitHorizontal()
         elif action == SPLIT_VERTICAL:     
-            self.splitVertical()
-        elif action == SKIP_REGION:        
-            self.skipRegion()
-        elif action == PLACE_LANDMARK:     
-            self.placeLandmark()
-
+            newbox = self.splitVertical()
+        #elif action == SKIP_REGION:        
+            #self.skipRegion()
+        #elif action == PLACE_LANDMARK:     
+            #self.placeLandmark()
+            
+        self.agent_window = newbox
         self.adjustAndClip()
+        r = self.ComputingReward()
+            
+        return r
         
      
 
 
     def MoveRight(self):
         
+        newbox = self.agent_window 
         boxW = self.agent_window[2] - self.agent_window[0]
         step = STEP_FACTOR * boxW
         # This action preserves box width and height
         if self.agent_window[2] + step < self.image_playground.shape[0]:
-            self.agent_window[0] += step
-            self.agent_window[2] += step
+            newbox[0] += step
+            newbox[2] += step
         else:
-            self.agent_window[0] = self.image_playground.shape[0] - boxW - 1
-            self.agent_window[2] = self.image_playground.shape[0] - 1 
+            newbox[0] = self.image_playground.shape[0] - boxW - 1
+            newbox[2] = self.image_playground.shape[0] - 1 
+            
+        return newbox
             
             
 
     def MoveDown(self):
         
+        newbox = self.agent_window 
         boxH = self.agent_window[3] - self.agent_window[1]
         step = STEP_FACTOR * boxH
         # This action preserves box width and height
         if self.agent_window[3] + step < self.image_playground.shape[1]:
-            self.agent_window[1] += step
-            self.agent_window[3] += step
+            newbox[1] += step
+            newbox[3] += step
         else:
-            self.agent_window[1] = self.image_playground.shape[1] - boxH - 1
-            self.agent_window[3] = self.image_playground.shape[1] - 1
+            newbox[1] = self.image_playground.shape[1] - boxH - 1
+            newbox[3] = self.image_playground.shape[1] - 1
+            
+        return newbox
         
         
         
@@ -132,6 +171,7 @@ class ObjLocaliser(object):
         
     def scaleUp(self):
         
+        newbox = self.agent_window 
         boxW = self.agent_window[2] - self.agent_window[0]
         boxH = self.agent_window[3] - self.agent_window[1]
         
@@ -151,16 +191,19 @@ class ObjLocaliser(object):
         
         widthChange = newDelta * boxW / 2.0
         heightChange = newDelta * boxH / 2.0
-        self.agent_window[0] -= widthChange
-        self.agent_window[1] -= heightChange
-        self.agent_window[2] += widthChange
-        self.agent_window[3] += heightChange
+        newbox[0] -= widthChange
+        newbox[1] -= heightChange
+        newbox[2] += widthChange
+        newbox[3] += heightChange
+        
+        return newbox
 
         
         
 
     def aspectRatioUp(self):
 
+        newbox = self.agent_window
         boxH = self.agent_window[3] - self.agent_window[1]
         boxW = self.agent_window[2] - self.agent_window[0]
         
@@ -180,43 +223,51 @@ class ObjLocaliser(object):
                 newDelta =  0.0
                 
         heightChange = newDelta * boxH / 2.0
-        self.agent_window[1] -= heightChange
-        self.agent_window[3] += heightChange
+        newbox[1] -= heightChange
+        newbox[3] += heightChange
+        
+        return newbox
 
         
         
     def MoveLeft(self):
         
+        newbox = self.agent_window
         boxW = self.agent_window[2] - self.agent_window[0]
         step = STEP_FACTOR * boxW
         
         # This action preserves box width and height
         if self.agent_window[0] - step >= 0:
-            self.agent_window[0] -= step
-            self.agent_window[2] -= step
+            newbox[0] -= step
+            newbox[2] -= step
         else:
-            self.agent_window[0] = 0
-            self.agent_window[2] = boxW
+            newbox[0] = 0
+            newbox[2] = boxW
         
+        return newbox
 
 
     def MoveUp(self):
 
+        newbox = self.agent_window
+        
         boxH = self.agent_window[3] - self.agent_window[1]
         
         step = STEP_FACTOR * boxH
         # This action preserves box width and height
         if self.agent_window[1] - step >= 0:
-            self.agent_window[1] -= step
-            self.agent_window[3] -= step
+            newbox[1] -= step
+            newbox[3] -= step
         else:
-            self.agent_window[1] = 0
-            self.agent_window[3] = boxH
+            newbox[1] = 0
+            newbox[3] = boxH
 
-            
+        return newbox
             
 
     def scaleDown(self):
+        
+        newbox = self.agent_window
         
         boxH = self.agent_window[3] - self.agent_window[1]
         boxW = self.agent_window[2] - self.agent_window[0]
@@ -236,30 +287,38 @@ class ObjLocaliser(object):
                 newDelta = MIN_BOX_SIDE /  boxH - 1
         widthChange = newDelta * boxW / 2.0
         heightChange = newDelta * boxH / 2.0
-        self.agent_window[0] += widthChange
-        self.agent_window[1] += heightChange
-        self.agent_window[2] -= widthChange
-        self.agent_window[3] -= heightChange
+        newbox[0] += widthChange
+        newbox[1] += heightChange
+        newbox[2] -= widthChange
+        newbox[3] -= heightChange
+        
+        return newbox
 
         
         
     def splitHorizontal(self):
         
+        newbox = self.agent_window
         boxW = self.agent_window[2] - self.agent_window[0]
         if boxW >  MIN_BOX_SIDE:
             half = boxW / 2.0
-            self.agent_window[2] -= half
+            newbox[2] -= half
+        return newbox
 
     def splitVertical(self): 
-
+        
+        newbox = self.agent_window
         boxH = self.agent_window[3] - self.agent_window[1]
         if boxH > MIN_BOX_SIDE:
             half = boxH/2.0
-            self.agent_window[3] -= half
+            newbox[3] -= half
+        return newbox
         
         
     def aspectRatioDown(self):
 
+        newbox = self.agent_window
+        
         boxW = self.agent_window[2] - self.agent_window[0]
         boxH = self.agent_window[3] - self.agent_window[1]
 
@@ -277,9 +336,9 @@ class ObjLocaliser(object):
             if ar < MIN_ASPECT_RATIO:
                 newDelta =  0.0
         widthChange = newDelta * boxW / 2.0
-        self.agent_window[0] -= widthChange
-        self.agent_window[2] += widthChange
-        
+        newbox[0] -= widthChange
+        newbox[2] += widthChange
+        return newbox
          
     def placeLandmark(self):
         self.landmarkIndex[ fingerprint(self.box) ] = self.box[:]
@@ -332,11 +391,43 @@ class ObjLocaliser(object):
                 self.agent_window[1] = 0
                 self.agent_window[3] = self.image_playground.shape[1] - 1
 
+                
+    def intersectionOverUnion(self, boxA, boxB):
+        
+        # determine the (x, y)-coordinates of the intersection rectangle
+        #boxA = self.agent_window
+        xA = max(boxA[0], boxB[0])
+        yA = max(boxA[1], boxB[1])
+        xB = min(boxA[2], boxB[2])
+        yB = min(boxA[3], boxB[3])
+
+        # compute the area of intersection rectangle
+        interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+        # compute the area of both the prediction and ground-truth
+        # rectangles
+        boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+        boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+        # compute the intersection over union by taking the intersection
+        # area and dividing it by the sum of prediction + ground-truth
+        # areas - the interesection area
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+
+        # return the intersection over union value
+        return iou  
+    
+    def ComputingReward(self):
+        
+        new_iou = self.intersectionOverUnion(self.agent_window, self.targets[0])
+        reward = new_iou - self.iou
+        self.iou = new_iou
+        return reward
+                
         
         
     def drawActions(self):
                 
-        im = np.array(Image.open('../VOC2012/JPEGImages/2010_000959.jpg'), dtype=np.uint8)
 
         # Create figure and axes
         fig,ax = plt.subplots(1)
@@ -352,7 +443,6 @@ class ObjLocaliser(object):
 
         plt.show()
 
-        
 
 
 
